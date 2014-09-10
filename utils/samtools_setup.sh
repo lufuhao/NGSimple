181b602c126b2ab563518cc6769b135cd1e81638
lufuhao <lufuhao@gmail.com>
2014-09-10 18:11:14 +0100

    SAMtools_install

--- utils/samtools_setup.sh
+++ utils/samtools_setup.sh
@@ -29,15 +29,19 @@ cd $RunDir/samtools/v$samtools_version/
 tar xvf $RunDir/samtools/v$samtools_version/src/$samtools_package > /dev/null
 mv samtools* $MachType
 cd $RunDir/samtools/v$samtools_version/$MachType
-make CXXFLAGS=-fPIC CFLAGS=-fPIC CPPFLAGS=-fPIC > make.log
+make CXXFLAGS+=" -fPIC" > make.log
 if [ ! -s ./samtools ]; then
   echo "Compiling Samtools failed"
   exit 0
 fi
 mkdir -p 'bin' 'include' 'lib' 'man/man1'
-cp ./samtools $RunDir/samtools/v$samtools_version/$MachType/bin/
+cp samtools misc/ace2sam misc/maq2sam-long misc/maq2sam-short misc/md5fa misc/md5sum-lite misc/wgsim misc/blast2sam.pl misc/bowtie2sam.pl misc/export2sam.pl misc/interpolate_sam.pl misc/novo2sam.pl misc/plot-bamstats misc/psl2sam.pl misc/sam2vcf.pl misc/samtools.pl misc/seq_cache_populate.pl misc/soap2sam.pl misc/varfilter.py misc/wgsim_eval.pl misc/zoom2sam.pl $RunDir/samtools/v$samtools_version/$MachType/bin/
+cp ./bcftools/bcftools $RunDir/samtools/v$samtools_version/$MachType/bin/
+cp ./bcftools/vcfutils.pl $RunDir/samtools/v$samtools_version/$MachType/bin/
 cp *.h $RunDir/samtools/v$samtools_version/$MachType/include/
+cp ./bcftools/*.h $RunDir/samtools/v$samtools_version/$MachType/include/
 cp *.a $RunDir/samtools/v$samtools_version/$MachType/lib/
+cp ./bcftools/libbcf.a $RunDir/samtools/v$samtools_version/$MachType/lib/
 cp samtools.1 $RunDir/samtools/v$samtools_version/$MachType/man/man1/
 if [ -d $RunDir/bin ]; then
   mkdir -p $RunDir/bin
